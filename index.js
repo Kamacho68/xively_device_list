5898d94c790779d38fe134a84569abe080318eb2
Kamel Aouidef <kamel.aouidef@logmein.com>
2017-12-11 17:41:55 +0000

    initial files commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// Use the environment variable or use a given port
+var PORT = process.env.PORT || 8080;
+
+var http = require('http');
+var fs = require('fs');
+
+var express = require('express');
+var server = express();
+
+/*server.use(express.static(__dirname + '/public'));*/
+
+/* 
+ * xi_device_list.html
+ * xi_index.html
+ * */
+fs.readFile('xi_index.html', function(err, html){
+	if(err){
+		throw err;
+	}
+	server = http.createServer(function (req, res){
+		res.statusCode = 200;
+		res.setHeader('Content-Type','text/html');
+		res.write(html);
+		res.end();
+	});
+	// Start the server
+	server.listen(PORT, function() {
+  		console.log('Server listening on: http://localhost:%s', PORT);
+	});
+});
